ccd5ff8aaff6b5afd27e623faf462383ad0ededb
mind1949 <lianjie1949@gmail.com>
2020-06-19 10:02:06 +0800

    20年6月19号学习结束

--- chapter1/chat/main.go
+++ chapter1/chat/main.go
@@ -32,7 +32,8 @@ func main() {
 	flag.Parse() // parse the flags
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
 	http.Handle("/room", r)
 	// get the room run
 	go r.run()
