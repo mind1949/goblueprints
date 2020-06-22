825df17f7c881975536ee5655e1a891153fef4af
mind1949 <lianjie1949@gmail.com>
2020-06-22 10:19:07 +0800

    20年6月22号学习结束

--- chapter1/chat/room.go
+++ chapter1/chat/room.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/gorilla/websocket"
 	"github.com/mind1949/goblueprints/chapter1/trace"
+	"github.com/stretchr/objx"
 )
 
 // room represents a chat room on which users can chat with each othter.
 type room struct {
 	// forward is a channel that holds incoming messages
 	// that should be forwarded to the other clients.
-	forward chan []byte
+	forward chan *message
 	// join is a channel for clients wishing to join the room
 	join chan *client
 	// leave is a channel for clients wishing to leave the room
@@ -27,7 +28,7 @@ type room struct {
 // newRoom makes a new room
 func newRoom() *room {
 	return &room{
-		forward: make(chan []byte),
+		forward: make(chan *message),
 		join:    make(chan *client),
 		leave:   make(chan *client),
 		clients: make(map[*client]bool),
@@ -73,10 +74,16 @@ func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		log.Fatal("ServeHTTP:", err)
 		return
 	}
+	authCookie, err := req.Cookie("auth")
+	if err != nil {
+		log.Fatal("Failed to get auth cookie:", err)
+		return
+	}
 	client := &client{
-		socket: socket,
-		send:   make(chan []byte, messageBufferSize),
-		room:   r,
+		socket:   socket,
+		send:     make(chan *message, messageBufferSize),
+		room:     r,
+		userData: objx.MustFromBase64(authCookie.Value),
 	}
 	r.join <- client
 	defer func() { r.leave <- client }()
